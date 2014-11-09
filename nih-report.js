880956be733f8ab18bd1f11a527bf182caea64f6
Tom Panning <tom@tomandreina.us>
2014-11-09 17:26:11 -0500

    Combine the query and aggregation boxes

--- nih-report.js
+++ nih-report.js
@@ -2,17 +2,29 @@
 
 var client = new $.es.Client({host: 'http://192.168.59.103:9200'});
 $(document).ready(function() {
-    $("#submit").on("click", function() {
-        var queryText = JSON.parse($("#query").val());
-        client.search({index:'nih', type:'small', body: queryText}).then(function(resp) {
-            var $results = $("#results");
+    $("#runsearch").on("click", function() {
+        client.search({index:'nih', type:'small', body: searchJSON}).then(function(resp) {
+        });
+    });
+
+    $("#runquery").on("click", function() {
+        var searchBody = {};
+        var searchText = $("#searchquery").val();
+        if (searchText) {
+            searchBody.query = JSON.parse(searchText);
+        }
+        var aggregationText = $("#aggregations").val();
+        if (aggregationText) {
+            searchBody.aggs = JSON.parse(aggregationText);
+        }
+        client.search({index:'nih', type:'small', body: searchBody}).then(function(resp) {
+            console.log(resp);
+            var $results = $("#searchresults");
             $results.empty();
-            $results.append("<ul>");
             _.forEach(resp.hits.hits, function(hit) {
                 var title = hit._source.title;
                 $results.append("<li>" + title + "</li>");
             });
-            $results.append("</ul>");
         });
     });
 });
