59802172ae85908dded0d4327b4fb0ed2ac35d7a
Tom Panning <tom@tomandreina.us>
2014-11-09 15:09:49 -0500

    Output the first 10 matches to a list

--- nih-report.js
+++ nih-report.js
@@ -4,9 +4,15 @@ var client = new $.es.Client({host: 'http://192.168.59.103:9200'});
 $(document).ready(function() {
     $("#submit").on("click", function() {
         var queryText = JSON.parse($("#query").val());
-        console.log(queryText);
         client.search({index:'nih', type:'small', body: queryText}).then(function(resp) {
-            console.log(resp.hits)
+            var $results = $("#results");
+            $results.empty();
+            $results.append("<ul>");
+            _.forEach(resp.hits.hits, function(hit) {
+                var title = hit._source.title;
+                $results.append("<li>" + title + "</li>");
+            });
+            $results.append("</ul>");
         });
     });
 });
